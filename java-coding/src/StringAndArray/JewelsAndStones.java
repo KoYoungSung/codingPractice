f5ccef7a94e3049b37125d1c93b72fac8574f7dc
Koyoungsung <ken3650@naver.com>
2020-11-03 21:30:25 +0900

    Jewels And Stones

---
+++ java-coding/src/StringAndArray/JewelsAndStones.java
@@ -0,0 +1,38 @@
+package StringAndArray;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsAndStones {
+    public static void main(String[] args) {
+        String J = "aA";
+        String S = "aAAbbbb";
+        System.out.println(solve(J,S));
+    }
+
+    private static int solve(String jew, String stone) {
+        Set<Character> set = new HashSet<>();
+        for (char jewel : jew.toCharArray()) {
+             set.add(jewel); // a,A
+        }
+
+        int count =0;
+        for (char stoneChar : stone.toCharArray()) {
+            System.out.println("stoneChar = " + stoneChar);
+            if (set.contains(stoneChar)) {
+                count++;
+            }
+        }
+        return count;
+                
+                
+    }
+}
+
+
+/*
+1) 보석은 대소문자 구분
+2) aA -> 2개
+3) 스톤에가서 aA가 개별적으로 몇개가 있는지 체크
+
+ */
