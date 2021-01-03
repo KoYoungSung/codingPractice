91dff1ca28b84e925baa707b7115da498e67c1e5
user-PC\user <ko>
2021-01-03 20:36:54 +0900

    no message

--- node.js/app.js
+++ node.js/app.js
@@ -1,5 +1,20 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
 app.listen(3000, function(){
     console.log("start express server on port 3000 !!")
+});
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/public/main.html")
+})
+
+app.post('/email_post',function(req,res){
+    console.log(req.body)
+    res.send("<h1>welcome " + req.body.email + "</h1>")
 })
